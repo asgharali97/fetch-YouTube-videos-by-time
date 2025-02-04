137016565b5dbdf90229f9237aebc549c1c86473
asgharali97 <asgharalifs48@gmail.com>
2025-02-03 21:36:24 -0800

    started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+  return (
+  <>
+  <div className="h-screen bg-gray-400">
+    Fetch youtube videos by using youtube API
+  </div> 
+  </>
+  );
+}
+
+export default App;
