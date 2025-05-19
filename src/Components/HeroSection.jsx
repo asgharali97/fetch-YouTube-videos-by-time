fd1b8598619f0d7ecdb84ae22391bbb989dc32ab
asgharali97 <asgharalifs48@gmail.com>
2025-05-18 23:50:22 -0700

    fix the button warring bug

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,20 +1,24 @@
-import { Button } from "@/components/ui/button";
+import { forwardRef } from "react";
 import { Form } from "./Form";
 import NavBar from './NavBar'
+import { Button } from "./ui/button";
+
+const SearchButton = forwardRef(function SearchButton({ className, children, ...props }, ref) {
+  return (
+    <Button
+      ref={ref}
+      className={`bg-[#DCD7C9] text-black hover:bg-[#fcfcfc] cursor-pointer ${className}`}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+});
+
 const HeroSection = () => {
-  // Button warrring sol:
-  // const SearchButton = React.forwardRef((props,ref) => {
-  //   <Button
-  //   ref={ref}
-  //   className="bg-[#DCD7C9] text-black hover:bg-[#fcfcfc] cursor-pointer"
-  //   {...props}
-  //   >
-  //     Start Searching
-  //   </Button>;
-  // });
   return (
     <>
-    <NavBar />
+      <NavBar />
       <div className="w-full py-4 px-2 mt-16">
         <div className="flex justify-center">
           <h1 className="text-3xl font-bold text-white flex flex-col items-center">
@@ -25,11 +29,9 @@ const HeroSection = () => {
         <div className="flex justify-center mt-8">
           <Form
             button={
-              <Button
-              className="bg-[#DCD7C9] text-black hover:bg-[#fcfcfc] cursor-pointer"
-              >
+              <SearchButton>
                 Start Searching
-              </Button>
+              </SearchButton>
             }
           />
         </div>
